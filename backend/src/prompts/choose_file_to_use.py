337fca95e671d93a81c485f3e95849bb677ced74
yehornepokrytyiwork@gmail.com <egorus1654@gmail.com>
2024-11-23 20:07:51 +0100

    update choose_file_to_use

--- backend/src/prompts/choose_file_to_use.py
+++ backend/src/prompts/choose_file_to_use.py
@@ -9,7 +9,7 @@ def choose_file_to_use(json_file_path:str) -> str:
         data = json.load(file)
     parsed_string = ""
     for d in data["datasets"]:
-        parsed_string += f"- Dataset_name: {d['name']}\n  Description: {d['description']}\n"
+        parsed_string += f"- Dataset_name: {d['Dataset_path']}\n  Description: {d['description']}\n"
 
     return f"""Based on the description of the files and the user's query, decide which one of the files should be used to answer a particular user query.
 Here is the data about the files contained in our dataset:\n{parsed_string}"""
