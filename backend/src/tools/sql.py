852560625029eec18b19c40bf210cfefc2666768
Michael Ruzmetov <mykhailo.ruzmetov@student.tuke.sk>
2024-11-23 14:33:59 +0100

    implement initial agent

---
+++ backend/src/tools/sql.py
@@ -0,0 +1,14 @@
+def sql_tool(query: str):
+    """
+    Executes an SQL query using the provided query string.
+
+    Args:
+        query (str): The SQL query to be executed.
+
+    Returns:
+        The result of the SQL query execution.
+    """
+    print(f"It works! {query}")
+    result = "Successful run :) Data: 1,2,3"
+
+    return result
